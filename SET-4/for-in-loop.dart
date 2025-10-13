1b28fefbf5a9493d604749a6b0d4392a406429ab
Helios012 <adithyapathakoti@gmail.com>
2025-10-14 00:41:56 +0530

    Create for-in-loop.dart

---
+++ SET-4/for-in-loop.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+void main() {
+  // List of numbers
+  List<int> numbers = [1, 2, 3, 4, 5];
+
+  print("Printing numbers using for-in loop:");
+
+  for (int number in numbers) {
+    print(number);
+  }
+}

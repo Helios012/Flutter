12f304c28f7cd749295e2a79c5e7a428a300d8d3
Helios012 <adithyapathakoti@gmail.com>
2025-10-14 00:20:23 +0530

    Create else-if.dart

---
+++ SET-2/else-if.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+void main() {
+  print("Enter your marks (0 - 100):");
+  int marks = int.parse(stdin.readLineSync()!);
+
+  if (marks >= 90) {
+    print("Grade: A+");
+  } else if (marks >= 75) {
+    print("Grade: A");
+  } else if (marks >= 60) {
+    print("Grade: B");
+  } else if (marks >= 40) {
+    print("Grade: C");
+  } else {
+    print("Grade: Fail");
+  }
+}
+
+//dart run  lib/else-if.dart
